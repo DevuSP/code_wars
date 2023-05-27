649d8083fc30f45e79256d878b58f603f5d97838
Devashish Singh Parmar <parmardevashish132@gmail.com>
2023-05-27 11:48:50 +0530

    Add files via upload

---
+++ Codewars/8kyu/Product_largest_two_no.js
@@ -0,0 +1,21 @@
+// Product of Largest Pair
+
+// Rick wants a faster way to get the product of the largest pair in an array. Your task is to create a performant solution to find the product of the largest two integers in a unique array of positive numbers.
+// All inputs will be valid.
+// Passing [2, 6, 3] should return 18, the product of [6, 3].
+
+// Disclaimer: only accepts solutions that are faster than his, which has a running time O(nlogn).
+
+// maxProduct([2, 1, 5, 0, 4, 3])              // 20
+// maxProduct([7, 8, 9])                       // 72
+// maxProduct([33, 231, 454, 11, 9, 99, 57])   // 104874
+
+// solution
+function maxProduct(a) {
+    // ...
+    let max1 = a.indexOf(Math.max(...a));
+    max1 = a.splice(max1,1);
+    let max2 = a.indexOf(Math.max(...a));
+    max2 = a.splice(max2,1);
+    return max1 * max2;
+  }
